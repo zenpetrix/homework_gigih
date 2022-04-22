6cf54e21724346ed651b1f399452676fe5c2c968
ZenPetrix <zenpetrix@gmail.com>
2022-04-23 03:47:24 +0700

    change redirect

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,8 +2,8 @@ import { generateRandomKey } from './helper';
 import { clearStorage } from './localstorage';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-//const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
-const REDIRECT_URI = https://homework-gigih-zen.vercel.app/login;
+// const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
+const REDIRECT_URI = process.env.REACT_APP_VERCEL_URL;
 const SCOPE = 'playlist-modify-private playlist-read-private';
 const STATE = generateRandomKey(16);
 
