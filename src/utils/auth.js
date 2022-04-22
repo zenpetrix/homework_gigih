194eb1ffda7750d644a7b687e551bced9e432d78
ZenPetrix <zenpetrix@gmail.com>
2022-04-23 04:08:25 +0700

    ganti redirect3

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,8 +2,7 @@ import { generateRandomKey } from './helper';
 import { clearStorage } from './localstorage';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-// const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
-const REDIRECT_URI = process.env.REACT_APP_VERCEL_URL;
+const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
 const SCOPE = 'playlist-modify-private playlist-read-private';
 const STATE = generateRandomKey(16);
 
